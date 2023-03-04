3a7093b33fe0b5189517ccad1936326e4d48f15d
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-03-04 12:34:58 +0000

    Update SongPlaylist.js

--- src/components/SongPlaylist.js
+++ src/components/SongPlaylist.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createRandomSong } from "../data";
 import { addSong } from "../store";
 
@@ -6,7 +6,9 @@ function SongPlaylist() {
   const dispatch = useDispatch();
   // To Do:
   // Get list of songs
-  const songPlaylist = [];
+  const songPlaylist = useSelector((state) => {
+    return state.songs;
+  });
 
   const handleSongAdd = (song) => {
     const action = addSong(song);
