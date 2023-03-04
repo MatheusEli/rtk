4a766d5cfc52511d17b28feedd69ea3eda0dfcfe
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-03-04 12:43:09 +0000

    Update SongPlaylist.js and index.js

--- src/store/index.js
+++ src/store/index.js
@@ -9,16 +9,17 @@ const songsSlice = createSlice({
     },
 
     removeSong(state, action) {
-      //
-    }
-  }
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
 });
 
 const store = configureStore({
   reducer: {
-    songs: songsSlice.reducer
-  }
+    songs: songsSlice.reducer,
+  },
 });
 
 export { store };
-export const { addSong } = songsSlice.actions;
+export const { addSong, removeSong } = songsSlice.actions;
